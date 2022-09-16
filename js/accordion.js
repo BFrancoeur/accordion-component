4737003587eab90adec69825a1b579c9cb422373
Brian Francoeur <francoeurbj@gmail.com>
2022-09-16 14:04:01 -0400

    debug javascript for +/- symbol display

--- js/accordion.js
+++ js/accordion.js
@@ -1,16 +1,12 @@
-const elemAccordionItemHeader = document.getElementById(
-  "accordion-item-header"
-);
-const elemObject = document.getElementById("accordion-item-toggle");
+const accordions = Array.from(document.querySelectorAll(".accordion"));
 
-const elemAccordionItemBody = document.getElementById("accordion-item-body");
+accordions.forEach((accordion) => {
+  const accordionHeader = accordion.querySelector(".accordion-header");
+  const accordionIcon = accordion.querySelector(".img");
+  const accordionBody = accordion.querySelector(".accordion-body");
 
-elemAccordionItemHeader.addEventListener("click", () => {
-  if (elemObject.getAttribute("data") === "icons/plus-solid.svg") {
-    elemObject.setAttribute("data", "icons/minus-solid.svg");
-    elemAccordionItemBody.classList.toggle("open");
-  } else {
-    elemObject.setAttribute("data", "icons/plus-solid.svg");
-    elemAccordionItemBody.classList.toggle("open");
-  }
+  accordionHeader.addEventListener("click", (event) => {
+    accordionIcon.classList.toggle("close");
+    accordionBody.classList.toggle("is-open");
+  });
 });
