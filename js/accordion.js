99d0e920331b3cf9c52288740448adb7f0d73c8e
Brian Francoeur <francoeurbj@gmail.com>
2022-09-19 14:22:15 -0400

    debug icon switching while opening or closing any one accordion

--- js/accordion.js
+++ js/accordion.js
@@ -2,11 +2,18 @@ const accordions = Array.from(document.querySelectorAll(".accordion"));
 
 accordions.forEach((accordion) => {
   const accordionHeader = accordion.querySelector(".accordion-header");
-  const accordionIcon = accordion.querySelector(".img");
+  const accordionIcon = accordion.querySelector(".accordion-icon");
   const accordionBody = accordion.querySelector(".accordion-body");
 
   accordionHeader.addEventListener("click", (event) => {
-    accordionIcon.classList.toggle("close");
+    if (accordionIcon.getAttribute("src") === "icons/plus-solid.svg") {
+      accordionIcon.src = "icons/minus-solid.svg";
+    } else {
+      return (accordionIcon.src = "icons/plus-solid.svg");
+    }
+  });
+
+  accordionHeader.addEventListener("click", (event) => {
     accordionBody.classList.toggle("is-open");
   });
 });
